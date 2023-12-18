0e36e806ab19250f89fc85ccc0b6cf82c730bc27
Sandro.dev <sandrosuarezaranda@gmail.com>
2023-12-18 17:16:02 +0100

    Página web completa

---
+++ js/localization.js
@@ -0,0 +1,50 @@
+let locale = "";
+const url = "../localization/localize.json";
+
+let traducciones = {};
+let variableRandom = {};
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            traducciones = data;
+            idiomaEspañol();
+        })
+        .catch(error => {
+            console.error('Error fetching the data:', error);
+        });
+
+    const inputEspañol = document.querySelector(".españa");
+    const inputIngles = document.querySelector(".ingles");
+
+    inputEspañol.addEventListener("click", idiomaEspañol);
+    inputIngles.addEventListener("click", idiomaIngles);
+});
+
+function traducirPagina() {
+    document
+        .querySelectorAll("[data-i18n-key]")
+        .forEach(translateElement);
+}
+
+function translateElement(element) {
+    const key = element.getAttribute("data-i18n-key");
+    const translation = traducciones[locale][key];
+    element.innerText = translation;
+}
+
+function idiomaEspañol() {
+    locale = "es";
+    traducirPagina();
+}
+
+function idiomaIngles() {
+    locale = "en";
+    traducirPagina();
+}

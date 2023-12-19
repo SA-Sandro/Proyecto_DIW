a79b27b516d58cdff5fb2ed68dd9bc66885fda62
Sandro <124067019+SA-Sandro@users.noreply.github.com>
2023-12-19 19:20:03 +0100

    Update localization.js

--- js/localization.js
+++ js/localization.js
@@ -5,7 +5,13 @@ let traducciones = {};
 let variableRandom = {};
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch(url)
+    fetch(url,{
+         method: 'GET',
+   headers: misCabeceras,
+   mode: 'no-cors', // <---
+   cache: 'default'
+
+    })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
